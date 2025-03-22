f914df2532eaaee9ffa6b3c3cffdda4c67abcf6a
Akhila-labs <akhilap3856@gmail.com>
2025-03-23 03:06:03 +0530

    Update output.tf

--- terraform/modules/load_balancer/output.tf
+++ terraform/modules/load_balancer/output.tf
@@ -1,9 +1,1 @@
-output "load_balancer_ip" {
-  description = "The IP address of the load balancer"
-  value       = google_compute_global_address.lb_ip.address
-}
 
-output "load_balancer_url" {
-  description = "The URL of the load balancer"
-  value       = "http://${google_compute_global_address.lb_ip.address}"
-}
