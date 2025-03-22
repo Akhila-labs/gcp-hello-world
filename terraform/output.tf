c7f9084281b3ca0b9ba9d0cf06e9da9b3fb3b4c2
Akhila-labs <akhilap3856@gmail.com>
2025-03-22 23:43:21 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,13 @@ output "function_name" {
   description = "The name of the Cloud Function"
   value       = module.cloud_function.function_name
 }
+
+output "cloud_function_url" {
+  description = "The URL of the deployed Cloud Function"
+  value       = module.cloud_function.function_url
+}
+
+output "load_balancer_ip" {
+  description = "The IP address of the load balancer"
+  value       = module.load_balancer.load_balancer_ip
+}
