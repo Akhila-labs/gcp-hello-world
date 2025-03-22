e95a82896fccb2a80291f0bd674286d2e69613a9
Akhila-labs <akhilap3856@gmail.com>
2025-03-23 03:09:26 +0530

    Update variables.tf

--- terraform/modules/security/variables.tf
+++ terraform/modules/security/variables.tf
@@ -2,3 +2,10 @@ variable "function_name" {
   description = "The name of the Cloud Function"
   type        = string
 }
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+
