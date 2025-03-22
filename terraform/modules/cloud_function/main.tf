e816dab6d824877f639a07e4c8bd4473fcdb5c57
Akhila-labs <akhilap3856@gmail.com>
2025-03-23 03:03:42 +0530

    Update main.tf

--- terraform/modules/cloud_function/main.tf
+++ terraform/modules/cloud_function/main.tf
@@ -1,15 +1,18 @@
+# Create a GCS bucket for the Cloud Function code
 resource "google_storage_bucket" "function_bucket" {
   name     = var.bucket_name
   location = var.region
 }
 
+# Upload the Cloud Function code (function.zip) to the GCS bucket
 resource "google_storage_bucket_object" "function_archive" {
   name   = "function.zip"
   bucket = google_storage_bucket.function_bucket.name
-  source = "${path.module}/../../cloud_function/function.zip" # Correct path
+  source = "${path.module}/../../cloud_function/function.zip" # Corrected path
   depends_on = [google_storage_bucket.function_bucket] # Ensure bucket is created first
 }
 
+# Deploy the Cloud Function
 resource "google_cloudfunctions_function" "hello_world" {
   name        = var.function_name
   runtime     = var.function_runtime
